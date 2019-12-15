02ed5040de6e61f229338b7c057ed21c4aa4e8b0
Ritesh <rdtailor@gmail.com>
2019-12-15 11:12:53 +0530

    Add express server and socket-io to test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client'
 import Board from './Components/Board';
 
-function App() {
-  return (
-    <Board/>
-  );
+// Making the App component
+class App extends Component {
+  constructor() {
+    super()    
+    const socket = socketIOClient(this.state.endpoint);
+    socket.on('action', (message) => {
+      console.log(message);
+    })
+  }
+
+    // sending sockets
+    send = () => {
+      const socket = socketIOClient(this.state.endpoint);
+      socket.emit('action', this.state.message) 
+    }
+  
+  render() {
+    return (
+      <div>
+        <Board/>
+        <button onClick={()=> this.send()}>send</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
